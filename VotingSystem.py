b16c9d602101dfa9346ab455cd49b5c8c51ba2dd
Methasha20250805@iit.ac.lk <methasha.20250805@iit.ac.lk>
2026-08-03 19:27:15 +0530

    editing

--- VotingSystem.py
+++ VotingSystem.py
@@ -73,7 +73,7 @@ def candidate_menu(cm: CandidateManager):
                 if choice == "1":
                     candidate_id = input("Candidate Id (10 digits): ").strip()
                     first_name = input("First Name (letters only, max 10 chars): ").strip()
-                    seat_number = input("Seat Number (2 digits, e.g. 01): ").strip()
+                    seat_number = input("Seat Number (only 2 digits, e.g. 01): ").strip()
                     candidate = cm.add_candidate(candidate_id, first_name, seat_number)
                     print(f"Candidate added successfully: {candidate}")
 
