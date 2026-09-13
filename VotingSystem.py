524b4a28c7fc6d73ba4e1abbd939ca3f298dc58c
Methasha20250805@iit.ac.lk <methashag@gmail.com>
2026-09-13 20:20:29 +0530

    editing

--- VotingSystem.py
+++ VotingSystem.py
@@ -17,7 +17,7 @@ def voter_menu(vm: VoterManager):
         print("\nVoter Registration")
         print("1.Add a Voter")
         print("2.Edit a Voter")
-        print("3.Search Voter")
+        print("3.Searching a Voter")
         print("4.Delete Voter")
         print("5.Back")
         choice = input("Choose an option: ").strip()
